e67effa43ed35a026a16ea03ad6946b60135d020
Samueltamawoki <samuelsenai1092@gmail.com>
2025-11-13 11:23:21 -0300

    Update post.js

--- public/post.js
+++ public/post.js
@@ -3,27 +3,22 @@ function enviarDados() {
     //Obter os valores dos campos informados pelo usuário
     var nome = document.getElementById('nome').value
     var sobrenome = document.getElementById('sobrenome').value
-    var idade = document.getElementById('idade').value
-    var rg = document.getElementById('rg').value
-    var cpf = document.getElementById('cpf').value
-    var rua = document.getElementById('rua').value
-    var numCasa = document.getElementById('numCasa').value
-    var cidade = document.getElementById('cidade').value
-    var bairo = document.getElementById('bairo').value
-    var estado = document.getElementById('estado').value
+    var cpf = document.getElementById('identificadorCPF').value
     var email = document.getElementById('email').value
-    var telefone = document.getElementById('tel').value
     var senha = document.getElementById('senha').value
-
+    var rua = document.getElementById('rua').value
+    var cep = document.getElementById('cep').value
+    var cidade = document.getElementById('cidade').value
+    var estado = document.getElementById('estado').value
+    var telefone = document.getElementById('telefone').value
 
     //Enviar um função que puxa o valor de uma API - FETCH
-    fetch('pessoas', {
+    fetch('http://localhost:3000/pessoas', {
         method: 'POST', //Metodo POST HTTP
         headers: {
             'Content-Type': 'application/json' //Tipo de conteudo enviado JSON
         },
-        body: JSON.stringify({nome: nome,sobrenome: sobrenome, idade: idade, rg: rg, cpf: cpf, rua: rua, numCasa : numCasa, cidade: cidade, bairo: bairo, estado: estado, email: email, telefone: telefone, senha: senha}) //Dados a serem enviados e convertidos
+        body: JSON.stringify({nome: nome, sobrenome: sobrenome, cpf: cpf, email: email, senha: senha, rua: rua, cep: cep, cidade: cidade, estado: estado, telefone: telefone}) //Dados a serem enviados e convertidos
     })
     .then(resposta => resposta.JSON) //Converte a resposta para JSON
-
 }
